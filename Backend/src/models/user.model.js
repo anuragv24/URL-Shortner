0552b285e12ae33a0f7771296d03b1b5c1d6c851
Anurag Verma <anuragv984@gmail.com>
2025-12-05 21:02:42 +0530

    Enhance user model with OTP functionality for verification process

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -3,13 +3,17 @@ import bcrypt from 'bcryptjs'
 import jwt from "jsonwebtoken"
 
 const userSchema = new mongoose.Schema({
-    name:{
+    name: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
     email: {
         type: String,
         required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
     },
     password: {
         type: String,
@@ -17,8 +21,35 @@ const userSchema = new mongoose.Schema({
     },
     referenceToken: {
         type: String,
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+
+    otpHash: {
+        type: String
+    },
+    otpExpiresAt: {
+        type: Date
+    },
+
+    otpFailedAttempts: {
+        type: Number,
+        default : 0
+    },
+    lastVerificationSentAt: {
+        type: Date
+    },
+
+    passwordResentTokenHash: {
+        type: String
+    },
+    passwordResetExpiresAt: {
+        type: Date
     }
-})
+
+}, {timestamps: true})
 
 userSchema.pre("save", async function(next){
     if(!this.isModified("password")) return next()
@@ -29,6 +60,37 @@ userSchema.pre("save", async function(next){
 
 userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.setOTP = async function(plainOTP, ttlMinutes = 10){
+    const saltRounds = 10;
+    this.otpHash = await bcrypt.hash(plainOTP, saltRounds)
+    this.otpExpiresAt = Date.now() + ttlMinutes*60*1000
+    this.lastVerificationSentAt = new Date()
+    this.otpFailedAttempts = 0
+    await this.save()
+}
+
+userSchema.methods.checkOTP = async function(plainOTP){
+    if(!this.otpHash || !this.otpExpiresAt) return false
+    if(Date.now() > this.otpExpiresAt) return 'expired'
+    const ok = await bcrypt.compare(plainOTP, this.otpHash)
+    if(!ok){
+        this.otpFailedAttempts = (this.otpFailedAttempts || 0) + 1
+        await this.save()
+        return false
+    }
+    //success
+    this.otpHash = undefined
+    this.otpExpiresAt = undefined
+    this.otpFailedAttempts = 0
+    this.isVerified = true
+    await this.save()
+    return true
+}
+
+userSchema.methods.isOTPCorrect = async function (otp){
+    return await bcrypt.compare(otp, this.otp)
 }
 
 userSchema.methods.generateAccessToken= function () {
@@ -55,6 +117,7 @@ userSchema.methods.generateRefreshToken = function () {
         }
     )
 }
+
 
 
 
