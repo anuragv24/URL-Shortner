a4d8a8b3e78dd74398b2e4302c63deaa3a1f3152
Anurag Verma <anuragv984@gmail.com>
2025-12-10 14:44:42 +0530

    Refactor vite.config.js; uncomment and clean up configuration for plugins and esbuild

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,12 +3,24 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  esbuild: {
-    drop: mode === 'production' ? ['console', 'debugger'] : []
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss()
+//   ],
+//   esbuild: {
+//     drop: mode === 'production' ? ['console', 'debugger'] : []
+//     }
+// })
+
+export default defineConfig(({ mode }) => {
+  return{
+    plugins: [
+      react(),
+      tailwindcss()
+    ],
+    esbuild: {
+      drop: mode === 'production' ? ['console', 'debugger'] : []
     }
+  }
 })
